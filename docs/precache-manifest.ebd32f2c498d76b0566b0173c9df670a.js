8108c5b5d34834fc96659953ca06a67e8cda703a
Nguyen Dac Doan <nguyendacdoan@gmail.com>
2022-04-19 11:33:52 +0700

    Update React

---
+++ docs/precache-manifest.ebd32f2c498d76b0566b0173c9df670a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9430765245188ca954e1b3c4f6c0b034",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ac02bf430f78dbe028b9",
+    "url": "./static/css/2.5ee7db17.chunk.css"
+  },
+  {
+    "revision": "fbdb26fd18e490a55836",
+    "url": "./static/css/main.ae187888.chunk.css"
+  },
+  {
+    "revision": "ac02bf430f78dbe028b9",
+    "url": "./static/js/2.dbde3b47.chunk.js"
+  },
+  {
+    "revision": "affaead7a68134b541afe1da4cf55fab",
+    "url": "./static/js/2.dbde3b47.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fbdb26fd18e490a55836",
+    "url": "./static/js/main.74a1d183.chunk.js"
+  },
+  {
+    "revision": "ab40382ca97013f04a71",
+    "url": "./static/js/runtime-main.6932692b.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7d24973d5f3a44c9916f390c500fd704.js"
+  "./precache-manifest.ebd32f2c498d76b0566b0173c9df670a.js"
 );
 
 self.addEventListener('message', (event) => {
