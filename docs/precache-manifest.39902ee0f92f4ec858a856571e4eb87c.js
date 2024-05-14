9569218d42250b6916785cd668022a2d21be2f7a
Nguyen Dac Doan <nguyendacdoan@gmail.com>
2024-05-14 16:42:22 +0700

    fs

--- docs/precache-manifest.39902ee0f92f4ec858a856571e4eb87c.js
+++ docs/precache-manifest.39902ee0f92f4ec858a856571e4eb87c.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "70a006271fea88899cfd032f4ec53eb4",
+    "revision": "4b0e8225cefc686f3c9e848cd9d11271",
     "url": "./index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/2.ed7e5119.chunk.css"
   },
   {
-    "revision": "c6c83eb912cc8f144af5",
+    "revision": "c6e363520cb6213060bb",
     "url": "./static/css/main.20579d6d.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.4f4d99a9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c6c83eb912cc8f144af5",
-    "url": "./static/js/main.62706103.chunk.js"
+    "revision": "c6e363520cb6213060bb",
+    "url": "./static/js/main.91b3b0de.chunk.js"
   },
   {
     "revision": "c7c2802b536706600249",
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9f948a1fc3d7cce044508239e4431c8c.js"
+  "./precache-manifest.39902ee0f92f4ec858a856571e4eb87c.js"
 );
 
 self.addEventListener('message', (event) => {
